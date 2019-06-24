38c6e0b68dc8c9897d65c0fa87fd0126bc8cef7f
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-06-24 16:56:49 -0300

    acrescentando classes security

--- src/main/java/br/gov/rn/saogoncalo/tributacaoapi/security/WebSecurityConfig.java
+++ src/main/java/br/gov/rn/saogoncalo/tributacaoapi/security/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/teste").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .httpBasic();
     }
 
     @Override //configurar arquivos estaticos da aplicação
